085bad7720b9469ee75454510f0f114e8d6ff66f
hiphonezhu <hiphonezhu@gmail.com>
2017-01-06 14:59:14 +0800

    AS2.2 CMake方式防止so被盗用

---
+++ NDKDemo/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,116 @@
+#include <jni.h>
+#include <string>
+
+const char *app_signature_sha1 = "67FC5A64399A44DC7083B696935D5333EEE8F1C8";
+const char HexCode[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
+                        'F'};
+
+extern "C"
+jstring
+Java_android_ndkdemo_MainActivity_stringFromJNI(
+        JNIEnv *env, jclass jcl, jobject context_object) {
+    std::string hello = "Hello from C++";
+
+    jclass context_class = env->GetObjectClass(context_object);
+
+    //context.getPackageManager()
+    jmethodID methodId = env->GetMethodID(context_class, "getPackageManager",
+                                          "()Landroid/content/pm/PackageManager;");
+    jobject package_manager_object = env->CallObjectMethod(context_object, methodId);
+    if (package_manager_object == NULL) {
+        return NULL;
+    }
+
+    //context.getPackageName()
+    methodId = env->GetMethodID(context_class, "getPackageName", "()Ljava/lang/String;");
+    jstring package_name_string = (jstring) env->CallObjectMethod(context_object, methodId);
+    if (package_name_string == NULL) {
+        return NULL;
+    }
+    env->DeleteLocalRef(context_class);
+
+    //PackageManager.getPackageInfo(Sting, int)
+    //public static final int GET_SIGNATURES= 0x00000040;
+    jclass pack_manager_class = env->GetObjectClass(package_manager_object);
+    methodId = env->GetMethodID(pack_manager_class, "getPackageInfo",
+                                "(Ljava/lang/String;I)Landroid/content/pm/PackageInfo;");
+    env->DeleteLocalRef(pack_manager_class);
+    jobject package_info_object = env->CallObjectMethod(package_manager_object, methodId,
+                                                        package_name_string, 0x40);
+    if (package_info_object == NULL) {
+        return NULL;
+    }
+    env->DeleteLocalRef(package_manager_object);
+
+    //PackageInfo.signatures[0]
+    jclass package_info_class = env->GetObjectClass(package_info_object);
+    jfieldID fieldId = env->GetFieldID(package_info_class, "signatures",
+                                       "[Landroid/content/pm/Signature;");
+    env->DeleteLocalRef(package_info_class);
+    jobjectArray signature_object_array = (jobjectArray) env->GetObjectField(package_info_object,
+                                                                             fieldId);
+    if (signature_object_array == NULL) {
+        return NULL;
+    }
+    jobject signature_object = env->GetObjectArrayElement(signature_object_array, 0);
+    env->DeleteLocalRef(package_info_object);
+
+    //Signature.toByteArray()
+    jclass signature_class = env->GetObjectClass(signature_object);
+    methodId = env->GetMethodID(signature_class, "toByteArray", "()[B");
+    env->DeleteLocalRef(signature_class);
+    jbyteArray signature_byte = (jbyteArray) env->CallObjectMethod(signature_object, methodId);
+
+    //new ByteArrayInputStream
+    jclass byte_array_input_class = env->FindClass("java/io/ByteArrayInputStream");
+    methodId = env->GetMethodID(byte_array_input_class, "<init>", "([B)V");
+    jobject byte_array_input = env->NewObject(byte_array_input_class, methodId, signature_byte);
+
+    //CertificateFactory.getInstance("X.509")
+    jclass certificate_factory_class = env->FindClass("java/security/cert/CertificateFactory");
+    methodId = env->GetStaticMethodID(certificate_factory_class, "getInstance",
+                                      "(Ljava/lang/String;)Ljava/security/cert/CertificateFactory;");
+    jstring x_509_jstring = env->NewStringUTF("X.509");
+    jobject cert_factory = env->CallStaticObjectMethod(certificate_factory_class, methodId,
+                                                       x_509_jstring);
+
+    //certFactory.generateCertificate(byteIn);
+    methodId = env->GetMethodID(certificate_factory_class, "generateCertificate",
+                                ("(Ljava/io/InputStream;)Ljava/security/cert/Certificate;"));
+    jobject x509_cert = env->CallObjectMethod(cert_factory, methodId, byte_array_input);
+    env->DeleteLocalRef(certificate_factory_class);
+
+    //cert.getEncoded()
+    jclass x509_cert_class = env->GetObjectClass(x509_cert);
+    methodId = env->GetMethodID(x509_cert_class, "getEncoded", "()[B");
+    jbyteArray cert_byte = (jbyteArray) env->CallObjectMethod(x509_cert, methodId);
+    env->DeleteLocalRef(x509_cert_class);
+
+    //MessageDigest.getInstance("SHA1")
+    jclass message_digest_class = env->FindClass("java/security/MessageDigest");
+    methodId = env->GetStaticMethodID(message_digest_class, "getInstance",
+                                      "(Ljava/lang/String;)Ljava/security/MessageDigest;");
+    jstring sha1_jstring = env->NewStringUTF("SHA1");
+    jobject sha1_digest = env->CallStaticObjectMethod(message_digest_class, methodId, sha1_jstring);
+
+    //sha1.digest (certByte)
+    methodId = env->GetMethodID(message_digest_class, "digest", "([B)[B");
+    jbyteArray sha1_byte = (jbyteArray) env->CallObjectMethod(sha1_digest, methodId, cert_byte);
+    env->DeleteLocalRef(message_digest_class);
+
+    //toHexString
+    jsize array_size = env->GetArrayLength(sha1_byte);
+    jbyte *sha1 = env->GetByteArrayElements(sha1_byte, NULL);
+    char *hex_sha = new char[array_size * 2 + 1];
+    for (int i = 0; i < array_size; ++i) {
+        hex_sha[2 * i] = HexCode[((unsigned char) sha1[i]) / 16];
+        hex_sha[2 * i + 1] = HexCode[((unsigned char) sha1[i]) % 16];
+    }
+    hex_sha[array_size * 2] = '\0';
+    //比较签名
+    if (strcmp(hex_sha, app_signature_sha1) == 0) {
+        return env->NewStringUTF(hello.c_str());
+    } else {
+        env->ThrowNew(jcl, "非法调用！");
+    }
+}
