460260a85cb9bbd4fdc4ec71696a6bcbf50ad0f5
hiphonezhu <hiphonezhu@gmail.com>
2017-01-16 12:23:57 +0800

    删除无用代码

--- AspectJDemo/app/src/main/java/android/aspectjdemo/PermissonAspect.java
+++ AspectJDemo/app/src/main/java/android/aspectjdemo/PermissonAspect.java
@@ -19,22 +19,19 @@ public class PermissonAspect {
 
     @Around("methodAnnotatedWithMPermisson(permisson)")
     public void checkPermisson(final ProceedingJoinPoint joinPoint, MPermisson permisson) throws Throwable {
+        // 权限
         String permissonStr = permisson.value();
 
-        // 一般使用栈顶Activity作为上下文
-        MainActivity mainActivity = (MainActivity) joinPoint.getThis();
+        // 模拟权限申请
+        MainActivity mainActivity = (MainActivity) joinPoint.getThis();  // 一般使用栈顶Activity作为上下文
         new AlertDialog.Builder(mainActivity).setTitle("提示")
                 .setMessage(permissonStr)
-                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                })
+                .setNegativeButton("取消", null)
                 .setPositiveButton("允许", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         try {
+                            // 继续执行原方法
                             joinPoint.proceed();
                         } catch (Throwable throwable) {
                             throwable.printStackTrace();
